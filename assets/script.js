bc6c4f78a18b3081182a0e499eb7d6529c69b81d
cory neel <corycneel@gmail.com>
2021-01-09 16:27:33 -0600

    added if else loops, which for some reason is causing my todaysDate VAR to stop functioning.  Need to find out why

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,27 @@ $(".saveBtn").on("click", function () {
             var currentTime = moment().hour();
         }
 
+//define variable for if else loops
+$(".time-block").each(function () {
+    var rowTime = parseInt($(this).attr("id").split("hour")[1]);
 
+//set if else loops to call state of time in each row and color coding for state of task
+            if (rowTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (rowTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
 
-//set if else loops to check time and determine in it is past, present or future
 //set function to grab info from local storage 
