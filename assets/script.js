3bf48d970aa212cb23f5f1acfd20c4d6f7fbc8de
cory neel <corycneel@gmail.com>
2021-01-09 20:56:07 -0600

    removed an unecessary declaration and added getTime to end of JS, now date shows in header again, added some CSS to header

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,40 @@
 //show current date and time in the header section
 var todaysDate=moment().format('dddd, MMMM Do YYYY');
-$("#currentDay").html(todaysDate);
+    $("#currentDay").html(todaysDate);
+    $(document).ready(function () {
 
 //listener for archive button click 
 $(".saveBtn").on("click", function () {
         
-    //get values of the timeblock content and time then save to local storage
-            var blockcontent = $(this).siblings(".description").val();
-            var time = $(this).parent().attr("id");
-                localStorage.setItem(time, blockcontent);
-        })
-        function getTime() {
-            var currentTime = moment().hour();
-        }
-
+//get values of the timeblock content and time then save to local storage
+var blockcontent = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id");
+    localStorage.setItem(time, blockcontent);})
+      function getTime() {
+            
 //define variable for if else loops
 $(".time-block").each(function () {
     var rowTime = parseInt($(this).attr("id").split("hour")[1]);
 
 //set if else loops to call state of time in each row and color coding for state of task
-            if (rowTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (rowTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
+if (rowTime < currentTime) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+}
+else if (rowTime === currentTime) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+}
+else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+}})}
 
-//set function to grab info from local storage 
+
+getTime();})
+
+
+
